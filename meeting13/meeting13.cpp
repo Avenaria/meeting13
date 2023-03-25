c41f981136c563878744e97a8c5ca05b26f7a47a
1 <1@LAPTOP-8U2CR0NO>
2023-03-25 18:39:02 +0300

    Добавьте файлы проекта.

---
+++ meeting13/meeting13.cpp
@@ -0,0 +1,14 @@
+﻿
+
+#include <iostream>
+#include <string>
+using  namespace std;
+
+int main()
+{
+    string str =" ([x-y-z}*[x+2y)-{z+4x)].";//-
+    string str2 = " : ({x-y-z}*[x+2y]-(z+4x))";// Правильная строка 
+    // потом доделаю 
+
+
+}
